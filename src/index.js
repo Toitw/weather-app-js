38214272c55a06bbcc4d5ecdad1d1f8240319e00
juroga_new <juanjrgast@gmail.com>
2023-11-09 17:38:09 +0100

    Added some more info and modified the info display so it is not visible until the api is called

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import './index.js';
 import './example.json';
+// import weatherIcon from './weather/64x64/day/113.png'; // Import the image
 
-console.log('Hello World!');
+// // Then, use the imported image in your code
+// const img = document.getElementById('weatherIcon');
+// img.src = weatherIcon; // Set the image source
--- src/ui.js
+++ src/ui.js
@@ -7,10 +7,14 @@ const locationName = document.getElementById('locationName');
 const temperature = document.getElementById('temperature');
 const weatherDescription = document.getElementById('weatherDescription');
 const errorMessage = document.getElementById('errorMessage');
+const windSpeed = document.getElementById('windSpeed');
+const humidity = document.getElementById('humidity');
 
 const displayWeather = (weatherData) => {
   locationName.textContent = weatherData.location.name;
   temperature.textContent = `Temperature: ${weatherData.current.temp_c}°C`;
+  windSpeed.textContent = `Wind Speed: ${weatherData.current.wind_kph} km/h`;
+  humidity.textContent = `Humidity: ${weatherData.current.humidity}%`;
   weatherDescription.textContent = `Description: ${weatherData.current.condition.text}`;
   errorMessage.textContent = ''; // Clear any previous error messages
 };
@@ -28,6 +32,7 @@ searchButton.addEventListener('click', async () => {
   const location = locationInput.value;
   try {
     weatherData = await API.fetchWeatherData(location); // Fetching weather data
+    weatherDisplay.style.display = 'block'; // Display the weather data
     displayWeather(weatherData); // Display the fetched weather data
   } catch (error) {
     displayMessage('Failed to fetch weather data');
